e99ef828d7e52e8e0b3bf8331780f51a6c308ba1
sedrik <chipiga86@mail.ru>
2012-03-22 00:44:48 +0400

    add main_page_action

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
 
   $('#sign_up').click(function(){
-    alert('hello');
+    $('.registration').css('display', 'block');
+  });
+
+  $('#recovery_pswd').click(function(){
+    $('.recovery').css('display', 'block');
+  });
+
+//********* Registration **************
+
+  $('#registration_close').click(function(){
+    $('.registration').css('display', 'none');
+  });
+
+//********* Recovery ******************
+
+  $('#recovery_close').click(function(){
+    $('.recovery').css('display', 'none');
   });
 
 });
