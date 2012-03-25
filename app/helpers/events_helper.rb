c137abbd3c4e9aa9ef99cff6a0327b5433994bf0
sedrik <chipiga86@mail.ru>
2012-03-25 21:58:20 +0400

    debug for heroku

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -15,7 +15,7 @@ module EventsHelper
     request = "point_date < :date AND cycle = 'monthly' AND EXTRACT(MONTH FROM point_date) = :day"
     events += current_user.events.select('id, title').where(request, { :date => date, :day => date.day })
 
-    request = 'point_date < :date AND cycle = "yearly" AND point_date LIKE "%-" || EXTRACT(MONTH FROM :date) || "-" || EXTRACT(DAY FROM :date)'
+    request = "point_date < :date AND cycle = 'yearly' AND point_date LIKE '%-' || EXTRACT(MONTH FROM :date) || '-' || EXTRACT(DAY FROM :date)"
     events += current_user.events.select('id, title').where(request, { :date => date })
     
     return events
